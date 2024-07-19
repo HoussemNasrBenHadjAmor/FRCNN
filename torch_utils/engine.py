fixing precision and recall computation

torch_utils/engine.py
@@ -150,7 +150,6 @@ def evaluate(
     pred_cls = []
     target_cls = []
     fn_count = 0
-    fn_count_v2 = 0
 
     n_threads = torch.get_num_threads()
     # FIXME remove this and make paste_masks_in_image run on the GPU
@@ -206,15 +205,10 @@ def evaluate(
                 pred_cls.append(pred_labels[i])
 
             target_cls.extend(gt_labels)
-            # Calculate false negatives
-            # False negative is (1 - True_positive)
-            fn_count_v2 += 1 - tp.cumsum(0)
-            print(f'fn_count_v2 : {fn_count_v2}')
-            fn_count += len(gt_labels) - len(detected)
-            print(f'fn_count : {fn_count}')
-        
-        model_time = time.time() - model_time
 
+            fn_count += len(gt_labels) - len(detected)
+    
+        model_time = time.time() - model_time
         res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
         evaluator_time = time.time()
         coco_evaluator.update(res)
@@ -231,22 +225,12 @@ def evaluate(
 
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
-    #print("Averaged stats:", metric_logger)
     coco_evaluator.synchronize_between_processes()
-    
-    #print(f'conf : {conf}')
-    #print(f'pred_cls : {pred_cls}')
-    #print(f'target_cls : {target_cls}')
-    #print(f'tp : {tp}')
-    #print(f'counter : {counter}')
-
-
 
     # accumulate predictions from all images
     coco_evaluator.accumulate()
     stats = coco_evaluator.summarize()
     torch.set_num_threads(n_threads)
-    #print(f'stats : {stats}')
     category_ids = data_loader.dataset.get_category_ids()
     category_names = data_loader.dataset.get_category_names()
 

train.py
@@ -24,7 +24,7 @@ from utils.general import (
     set_training_dir, Averager, 
     save_model, save_loss_plot, save_box_loss, save_cls_loss, save_dfl_loss, save_precision, save_recal, save_map50, save_map50_95, save_precision_recall_curve, save_precision_confidence_curve, save_recall_confidence_curve, save_f1_confidence_curve,
     show_tranformed_image,
-    save_mAP, save_model_state, SaveBestModel, save_precisionB
+    save_mAP, save_model_state, SaveBestModel, save_precisionB, save_recallB
 )
 from utils.logging import (
     set_log, 
@@ -294,17 +294,12 @@ def main(args):
             colors=COLORS
         )
 
-        print(f'stats : {stats}')
-
         precision = calculate_precision(tp, pred_cls)
         recall = calculate_recall(tp, fn_count)
-        
-        # Append the precision value
+
+        # Append the precision & recall values
         val_precision_per_epoch.append(precision)
         val_recall_per_epoch.append(recall)
-        print(f'precision_per_epoch : {val_precision_per_epoch}')
-        print(f'val_recall_per_epoch : {val_recall_per_epoch}')
-
 
         # Append the current epoch's batch-wise losses to the `train_loss_list`.
         train_loss_list.extend(batch_loss_list)
@@ -328,6 +323,8 @@ def main(args):
         ap_per_class(category_names,np_tp, np.array(conf), np.array(pred_cls), np.array(target_cls), plot=True , save_dir = OUT_DIR)
         
 
+
+
         # Save box loss for each epoch
         save_box_loss(all_train_box_loss_per_epoch, OUT_DIR, title='Box_Regression_Loss_Per_Epoch')
         # Save class loss for each epoch 
@@ -343,6 +340,7 @@ def main(args):
         # Save mAP50_95  for each epoch
         save_map50_95(val_mAP50_95, OUT_DIR, title='Metrics-mAP50-95(B)')
         save_precisionB(val_precision_per_epoch, OUT_DIR, title='Metrics-precision(B)')
+        save_recallB(val_recall_per_epoch , OUT_DIR, title = 'Metrics-recall(B)')
         # Save precision recall curve for each epoch
         #save_precision_recall_curve(coco_evaluator, category_ids, category_names, OUT_DIR, 'Precision-Recall Curve')
         # Save precision confidence curve for each epoch